4834923fd03130bce4e1a0c3aed90f43cacdb035
xinru123 <zxr1318472595@126.com>
2018-10-15 14:45:14 +0800

    weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,182 @@
+<template>
+    <div class="HomeRecommend">
+          <div class="title">
+            <span  class="iconfont heart">&#xe6aa;</span>
+          猜你喜欢</div>
+          <ul>
+            <li class="item" v-for="x of recommendList" :key="x.id">
+              <div class="item-img-wrapper">
+                <img class="item-img" :src="x.imgUrl" alt="">      
+              </div>
+              <div class="item-info">
+                <p class="item-title">{{x.title}}</p>
+                <div class="item-two">
+                  <div class="star iconfont" v-html="x.icon"></div>
+                  <div class="star iconfont" v-html="x.icon"></div>
+                  <div class="star iconfont" v-html="x.icon"></div>
+                  <div class="star iconfont" v-html="x.icon"></div>
+                  <span class="comment">{{x.num}}条评论</span>
+                </div>
+                <div class="item-three">
+                  <strong class="price">{{x.price}}</strong>
+                  <span class="local">{{x.local}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg",
+          title: "古北水镇",
+          icon: "&#xe810;",
+          num: "3267",
+          price: "￥141起",
+          local: "密云县"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1606/36/362f693e73d95311e2.water.jpg_200x200_c11e4e13.jpg",
+          title: "喇叭沟原始森林公园",
+          icon: "&#xe810;",
+          num: "3267",
+          price: "￥41起",
+          local: "怀柔区"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1603/85/85c883189bf7293390.water.jpg_200x200_3165123d.jpg",
+          title: "什刹海",
+          icon: "&#xe810;",
+          num: "3267",
+          price: "￥61起",
+          local: "西城区"
+        },
+        {
+          id: "0004",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/d2/d274c92de14c93da.water.jpg_200x200_2cce447f.jpg ",
+          title: "颐和园",
+          icon:"&#xe810;",
+          num: "3267",
+          price: "￥160起",
+          local: "海淀区"
+        },
+        {
+          id: "0005",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1803/7b/7b023b9728f0834fa3.img.jpg_200x200_5fddc1d8.jpg",
+          title: "世界花卉大观园",
+          icon: "&#xe810;",
+          num: "3267",
+          price: "￥99起",
+          local: "丰台区"
+        },
+        {
+          id: "0006",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1604/78/78873494f26e554090.water.jpg_200x200_331422ad.jpg",
+          title: "京东石林峡",
+          icon: "&#xe810;",
+          num: "3267",
+          price: "￥61起",
+          local: "平谷区"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+@import '~styles/varibles.styl';
+.title 
+  line-height: 0.9rem;
+  text-indent: 0.2rem;
+  margin-top: 0.2rem;
+  .heart
+   color :#ff585b
+
+.item 
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  height: 2.4rem;
+  overflow: hidden;
+  padding-top: 0.2rem;
+  padding-bottom: 0.2rem;
+  margin-left: 0.2rem;
+
+  .item-img-wrapper 
+    width: 2rem;
+    height: 2rem;
+    flex: 1;
+
+    .item-img 
+      width: 2rem;
+      height: 2rem;
+    
+  
+
+  .item-info 
+    flex: 2;
+    float: left;
+    padding-left: 0.2rem;
+
+    .item-title 
+      height: 0.4rem;
+      width: auto;
+      margin-top: 0.26rem;
+      font-size: 0.32rem;
+    
+
+    .item-two 
+      height: 0.34rem;
+      line-height: 0.34rem;
+      text-align: center;
+      margin-top: 0.14rem;
+
+      .star 
+        height: 0.28rem;
+        float: left;
+        color: #FFB436;
+      
+
+      .comment 
+        margin-left :.2rem
+        float: left;
+        color: #616161;
+      
+    
+
+    .item-three 
+      height: 0.46rem;
+      margin-top: 0.22rem;
+      line-height: 0.46rem;
+      text-align: center;
+
+      .price 
+        float: left;
+        color: $recommendPriceColor
+        font-size: 0.4rem;
+      
+
+      .local 
+        float: right;
+        margin-right: 0.24rem;
+        color: $descColor;
+      
+    
+  
+
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="HomeRecommend">
+          <div class="title">周末去哪儿</div>
+          <ul>
+            <li class="item" v-for="x of recommendList" :key="x.id">
+              <div class="item-img-wrapper">
+                <img class="item-img" :src="x.imgUrl" alt="">      
+              </div>
+              <div class="item-info">
+                <p class="item-title">{{x.title}}</p>
+                <p class="item-desc">{{x.desc}}</p>
+              </div>
+            </li>
+          </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: "HomeWeekend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg",
+          title: "上海必游TOP10",
+          desc: "中西合璧，现代和传统各有各的精彩"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1603/ad/69b0fc5f48ad5f.jpg_r_640x214_b0fd5c7b.jpg",
+          title: "亲子时光TOP10",
+          desc: "走进自然或玩转乐园，感知大千世界"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1511/1d/fe098e8cfec117.jpg_r_640x214_a9be292a.jpg",
+          title: "上海城市观光",
+          desc: "来这里寻找你所认识的上海"
+        },
+        {
+          id: "0004",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/7b/2a6b0ce087f0e5.jpg_r_640x214_5c76b115.jpg",
+          title: "情迷博物馆",
+          desc: "穿梭时光岁月寻找答案"
+        },
+        {
+          id: "0005",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/b7/2f4621a3a4e12c.jpg_r_640x214_d55c10ba.jpg",
+          title: "学生最爱TOP10",
+          desc: "学习之余最爱玩耍，爱玩的你不可错过"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+@import '~styles/varibles.styl';
+.title 
+  line-height: 0.9rem;
+  text-indent: 0.2rem;
+  margin-top: 0.2rem;
+  background :$weekendTitle
+.item
+  height :4.04rem
+  width :100%
+  margin-bottom :.1rem
+  .item-img-wrapper 
+    width: 100%;
+    height: 0;
+    padding-bottom :2.8rem
+    .item-img 
+      width: 100%;
+      height: 2.78rem;
+    
+  .item-info 
+    padding:.14rem .2rem .2rem .2rem ;
+    .item-title 
+      padding-right: 1.4rem;
+      color:$itemTitleColor
+      font-size: .28rem;
+      line-height: .48rem;
+      ellipsis()
+    .item-desc 
+      padding-right: 1.4rem;
+      color:$descColor;
+      font-size: .24rem;
+      line-height: .42rem;
+      ellipsis()
+</style>
